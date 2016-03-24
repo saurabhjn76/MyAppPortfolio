b195147f4fe1c049921d4e9836c975fe25891833
saurabhjn76 <saurabhjn76@gmail.com>
2016-03-24 16:09:31 +0530

    Added toast msg for the button

--- app/src/main/java/com/mathapp/saurabhjn76/myappportfolio/MainActivity.java
+++ app/src/main/java/com/mathapp/saurabhjn76/myappportfolio/MainActivity.java
@@ -1,13 +1,17 @@
 package com.mathapp.saurabhjn76.myappportfolio;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +21,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button btn;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        for(int i=1;i<=6;i++)
+        {
+            int id = getResources().getIdentifier("button"+i, "id", getPackageName());
+            btn = (Button) findViewById(id);
+            btn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Button b = (Button) view;
+                    String message = String.format(getResources().getString(R.string.launchmsg), b.getText().toString());
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+        }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
